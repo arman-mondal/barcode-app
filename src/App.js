810108b7cb6b28862d2578311d075760166f6984
arman-mondal <arman@armanmondal.me>
2024-08-03 00:03:17 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Barcode from 'react-barcode';
+import { useState } from 'react';
 function App() {
+  const [format,setformat]=useState("CODE128");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Barcode value="240726001" format={format}  />
+     <h1>{format}</h1>
+     <select value={format} onChange={(a)=>setformat(a.target.value)} >
+        <option value="CODE128" >CODE128</option>
+        <option value="CODE128A" >CODE128A</option>
+        <option value="CODE128B" >CODE128B</option>
+        <option value="CODE128C" onClick={()=>setformat("CODE128C")}>CODE128C</option>
+
+        <option value="CODE39" >CODE39</option>
+        <option value="EAN13" >EAN13</option>
+        <option value="EAN8" >EAN8</option>
+        <option value="UPC" >UPC</option>
+        <option value="ITF14">ITF14</option>
+        <option value="ITF" >ITF</option>
+        <option value="MSI" >MSI</option>
+        <option value="MSI10">MSI10</option>
+        <option value="MSI11" >MSI11</option>
+        <option value="MSI1010" >MSI1010</option>
+        <option value="MSI1110">MSI1110</option>
+        
+     </select>
     </div>
   );
 }
